ce2590939131826869c621115be47893cd9e1433
jakwuh <jakwuh@gmail.com>
2019-03-09 14:35:52 +0300

    Init repo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { AgGridReact } from 'ag-grid-react';
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-class App extends Component {
+const columnDefs = [
+  {
+    headerName: "Name",
+    cellRendererFramework: ({ data }) => data.name,
+    width: 120,
+    autoHeight: true,
+    cellStyle: { 'white-space': 'normal !important' }
+  },
+];
+
+let i = 0;
+
+class GridExample extends React.Component {
+  constructor(props) {
+    super(props)
+
+    document.querySelector('#add').onclick = () => {
+      this.setState((state) => ({
+        rowData: [
+          ...state.rowData,
+          { id: ++i, name: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus reiciendis dicta voluptatem ea culpa cum quisquam aspernatur. Officia eveniet obcaecati tempora doloremque, voluptas accusantium, id quia odio quidem, doloribus veniam.' },
+          { id: ++i, name: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus reiciendis dicta voluptatem ea culpa cum quisquam aspernatur. Officia eveniet obcaecati tempora doloremque, volupta accusantium, id quia odio quidem, doloribus veniam.' },
+          { id: ++i, name: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus reiciendis dicta voluptatem ea culpa cum quisquam aspernatur. Officia eveniet obcaecati tempora doloremque, voluptas accusantium, id quia odio quidem, doloribus veniam.' },
+        ]
+      }))
+    }
+
+    this.state = {
+      rowData: [],
+    };
+  }
+
+  onGridReady({ api }) {
+    this.gridApi = api;
+    api.resetRowHeights();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ width: "100%", height: "500px" }}>
+        <div
+          id="myGrid"
+          style={{
+            boxSizing: "border-box",
+            height: "100%",
+            width: "100%"
+          }}
+          className="ag-theme-balham"
+        >
+          <AgGridReact
+            columnDefs={columnDefs}
+            debug={false}
+            rowData={this.state.rowData}
+            getRowNodeId={item => item.id}
+            deltaRowDataMode
+            rowHeight={15}
+          />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default GridExample;
